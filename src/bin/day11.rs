23eb378f91c7a950a03601a793f88e65b0e05a63
Daniel Cheng <zetafunction@gmail.com>
2023-12-10 23:33:22 -0800

    Day 11: add faster adjustment lookup and memoized adjusted galaxies.

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -15,15 +15,16 @@
 use aoc_2023::geometry::Point2;
 use aoc_2023::oops::Oops;
 use aoc_2023::time;
-use std::collections::BTreeSet;
+use std::collections::{BTreeMap, BTreeSet};
 use std::io::{self, Read};
 use std::str::FromStr;
 
 #[derive(Debug)]
 struct Puzzle {
     galaxies: Vec<Point2>,
-    empty_cols: BTreeSet<i32>,
-    empty_rows: BTreeSet<i32>,
+    // Map of empty cols/rows to the number of adjustments needed.
+    empty_cols: BTreeMap<i32, i32>,
+    empty_rows: BTreeMap<i32, i32>,
 }
 
 impl FromStr for Puzzle {
@@ -47,14 +48,18 @@ impl FromStr for Puzzle {
         let height = height + 1;
         let width = height;
 
-        // Find empty rows and columns first.
-        let mut empty_cols = (0i32..width).collect::<BTreeSet<_>>();
-        let mut empty_rows = (0i32..height).collect::<BTreeSet<_>>();
+        // (-1, -1) should never be a valid coordinate, but removes an edge case when looking up
+        // how many adjustments are needed later.
+        let mut empty_cols = (-1i32..width).collect::<BTreeSet<_>>();
+        let mut empty_rows = (-1i32..height).collect::<BTreeSet<_>>();
 
         for galaxy in &galaxies {
             empty_cols.remove(&galaxy.x);
             empty_rows.remove(&galaxy.y);
         }
+
+        let empty_cols = std::iter::zip(empty_cols.into_iter(), 0i32..).collect();
+        let empty_rows = std::iter::zip(empty_rows.into_iter(), 0i32..).collect();
 
         Ok(Puzzle {
             galaxies,
@@ -68,23 +73,41 @@ fn parse(input: &str) -> Result<Puzzle, Oops> {
     input.parse()
 }
 
-fn adjust_point_for_expansion_factor(puzzle: &Puzzle, point: Point2, factor: usize) -> Point2 {
+fn adjust_point_for_expansion_factor(puzzle: &Puzzle, point: &Point2, factor: i32) -> Point2 {
+    let x_adj = puzzle
+        .empty_cols
+        .range(-1..=point.x)
+        .rev()
+        .next()
+        .unwrap()
+        .1;
+    let y_adj = puzzle
+        .empty_rows
+        .range(-1..=point.y)
+        .rev()
+        .next()
+        .unwrap()
+        .1;
     Point2::new(
-        point.x
-            + i32::try_from(puzzle.empty_cols.range(0..point.x).count() * (factor - 1)).unwrap(),
-        point.y
-            + i32::try_from(puzzle.empty_rows.range(0..point.y).count() * (factor - 1)).unwrap(),
+        point.x + x_adj * (factor - 1),
+        point.y + y_adj * (factor - 1),
     )
 }
 
-fn solve_with_expansion_factor(puzzle: &Puzzle, factor: usize) -> u64 {
-    (0..puzzle.galaxies.len())
+fn solve_with_expansion_factor(puzzle: &Puzzle, factor: i32) -> u64 {
+    let memoized = puzzle
+        .galaxies
+        .iter()
+        .map(|galaxy| adjust_point_for_expansion_factor(puzzle, galaxy, factor))
+        .collect::<Vec<_>>();
+    (0..memoized.len())
         .flat_map(|i| {
-            (i + 1..puzzle.galaxies.len())
+            let memoized = &memoized;
+            (i + 1..memoized.len())
                 .map(move |j| {
-                    let src = adjust_point_for_expansion_factor(puzzle, puzzle.galaxies[i], factor);
-                    let dst = adjust_point_for_expansion_factor(puzzle, puzzle.galaxies[j], factor);
-                    Point2::manhattan_distance(&src, &dst)
+                    let src = &memoized[i];
+                    let dst = &memoized[j];
+                    Point2::manhattan_distance(src, dst)
                 })
                 .map(u64::from)
         })
