75dd86f9ca9ad0331bc34e44901d5491a4764605
Daniel Cheng <zetafunction@gmail.com>
2023-12-11 01:24:46 -0800

    Day 05: simplify parsing code.

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use aoc_2023::oops::Oops;
 use aoc_2023::time;
+use aoc_2023::{oops, oops::Oops};
 use std::collections::BTreeMap;
 use std::io::{self, Read};
 use std::str::FromStr;
@@ -24,109 +24,42 @@ struct Range {
 }
 
 struct Puzzle {
-    initial_seeds: Vec<u64>,
-    seeds_to_soils: BTreeMap<u64, Range>,
-    soils_to_fertilizers: BTreeMap<u64, Range>,
-    fertilizers_to_waters: BTreeMap<u64, Range>,
-    waters_to_lights: BTreeMap<u64, Range>,
-    lights_to_temperatures: BTreeMap<u64, Range>,
-    temperatures_to_humidities: BTreeMap<u64, Range>,
-    humidities_to_locations: BTreeMap<u64, Range>,
+    seeds: Vec<u64>,
+    mappings: Vec<BTreeMap<u64, Range>>,
 }
 
-fn parse_mapping(s: &str) -> Result<BTreeMap<u64, Range>, Oops> {
-    let mut mapping = BTreeMap::new();
-    for line in s.lines() {
-        let mut nums = line.split_whitespace().map(str::parse::<u64>);
-        let dest = nums.next().expect("dest missing")?;
-        let src = nums.next().expect("src missing")?;
-        let len = nums.next().expect("len missing")?;
-        mapping.insert(src, Range { dest, len });
-    }
-    Ok(mapping)
+fn parse_mappings(s: &str) -> Result<BTreeMap<u64, Range>, Oops> {
+    s.lines()
+        .skip(1)
+        .map(|line| {
+            let mut nums = line.split_whitespace().map(str::parse::<u64>);
+            let dest = nums.next().expect("dest missing")?;
+            let src = nums.next().expect("src missing")?;
+            let len = nums.next().expect("len missing")?;
+            Ok((src, Range { dest, len }))
+        })
+        .collect()
 }
 
 impl FromStr for Puzzle {
     type Err = Oops;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut sections = s.split("\n\n");
+        let (seeds, mappings) = s.split_once("\n\n").ok_or_else(|| oops!("bad input"))?;
 
-        let initial_seeds = sections
-            .next()
-            .expect("seeds")
+        let seeds = seeds
             .strip_prefix("seeds: ")
-            .expect("missing prefix")
+            .ok_or_else(|| oops!("missing seeds: prefix"))?
             .split_whitespace()
             .map(str::parse)
             .collect::<Result<_, _>>()?;
 
-        let seeds_to_soils = parse_mapping(
-            sections
-                .next()
-                .expect("missing section")
-                .strip_prefix("seed-to-soil map:\n")
-                .expect("missing prefix"),
-        )?;
+        let mappings = mappings
+            .split("\n\n")
+            .map(parse_mappings)
+            .collect::<Result<_, _>>()?;
 
-        let soils_to_fertilizers = parse_mapping(
-            sections
-                .next()
-                .expect("missing section")
-                .strip_prefix("soil-to-fertilizer map:\n")
-                .expect("missing prefix"),
-        )?;
-
-        let fertilizers_to_waters = parse_mapping(
-            sections
-                .next()
-                .expect("missing section")
-                .strip_prefix("fertilizer-to-water map:\n")
-                .expect("missing prefix"),
-        )?;
-
-        let waters_to_lights = parse_mapping(
-            sections
-                .next()
-                .expect("missing section")
-                .strip_prefix("water-to-light map:\n")
-                .expect("missing prefix"),
-        )?;
-
-        let lights_to_temperatures = parse_mapping(
-            sections
-                .next()
-                .expect("missing section")
-                .strip_prefix("light-to-temperature map:\n")
-                .expect("missing prefix"),
-        )?;
-
-        let temperatures_to_humidities = parse_mapping(
-            sections
-                .next()
-                .expect("missing section")
-                .strip_prefix("temperature-to-humidity map:\n")
-                .expect("missing prefix"),
-        )?;
-
-        let humidities_to_locations = parse_mapping(
-            sections
-                .next()
-                .expect("missing section")
-                .strip_prefix("humidity-to-location map:\n")
-                .expect("missing prefix"),
-        )?;
-
-        Ok(Puzzle {
-            initial_seeds,
-            seeds_to_soils,
-            soils_to_fertilizers,
-            fertilizers_to_waters,
-            waters_to_lights,
-            lights_to_temperatures,
-            temperatures_to_humidities,
-            humidities_to_locations,
-        })
+        Ok(Puzzle { seeds, mappings })
     }
 }
 
@@ -148,64 +81,33 @@ fn parse(input: &str) -> Result<Puzzle, Oops> {
 
 fn part1(puzzle: &Puzzle) -> u64 {
     puzzle
-        .initial_seeds
+        .seeds
         .iter()
         .map(|seed| {
-            do_mapping(
-                do_mapping(
-                    do_mapping(
-                        do_mapping(
-                            do_mapping(
-                                do_mapping(
-                                    do_mapping(*seed, &puzzle.seeds_to_soils),
-                                    &puzzle.soils_to_fertilizers,
-                                ),
-                                &puzzle.fertilizers_to_waters,
-                            ),
-                            &puzzle.waters_to_lights,
-                        ),
-                        &puzzle.lights_to_temperatures,
-                    ),
-                    &puzzle.temperatures_to_humidities,
-                ),
-                &puzzle.humidities_to_locations,
-            )
+            puzzle
+                .mappings
+                .iter()
+                .fold(*seed, |val, mapping| do_mapping(val, mapping))
         })
         .min()
         .expect("no seeds")
 }
 
 fn part2(puzzle: &Puzzle) -> u64 {
-    puzzle
-        .initial_seeds
-        .iter()
-        .step_by(2)
-        .zip(puzzle.initial_seeds.iter().skip(1).step_by(2))
-        .flat_map(|(seed, range)| {
-            (0..*range).map(|i| {
-                do_mapping(
-                    do_mapping(
-                        do_mapping(
-                            do_mapping(
-                                do_mapping(
-                                    do_mapping(
-                                        do_mapping(*seed + i, &puzzle.seeds_to_soils),
-                                        &puzzle.soils_to_fertilizers,
-                                    ),
-                                    &puzzle.fertilizers_to_waters,
-                                ),
-                                &puzzle.waters_to_lights,
-                            ),
-                            &puzzle.lights_to_temperatures,
-                        ),
-                        &puzzle.temperatures_to_humidities,
-                    ),
-                    &puzzle.humidities_to_locations,
-                )
-            })
+    std::iter::zip(
+        puzzle.seeds.iter().step_by(2),
+        puzzle.seeds.iter().skip(1).step_by(2),
+    )
+    .flat_map(|(seed, range)| {
+        (0..*range).map(move |i| {
+            puzzle
+                .mappings
+                .iter()
+                .fold(seed + i, |val, mapping| do_mapping(val, mapping))
         })
-        .min()
-        .expect("no seeds")
+    })
+    .min()
+    .expect("no seeds")
 }
 
 fn main() -> Result<(), Oops> {
